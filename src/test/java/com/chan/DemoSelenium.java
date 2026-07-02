dbcf493c7b1d76322ea5b32af948e5c0686cc893
Chan <jack22111998@gmail.com>
2026-07-02 22:41:39 +0700

    Add Selenium Java
    Add selenium java to project

---
+++ src/test/java/com/chan/DemoSelenium.java
@@ -0,0 +1,14 @@
+package com.chan;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoSelenium {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/1");
+
+        driver.quit();
+    }
+}
